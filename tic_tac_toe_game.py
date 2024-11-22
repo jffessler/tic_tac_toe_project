6bb4f5d51ea50c5745a4871f4ee22df791654a37
John Fessler <jffessler@gmail.com>
2024-11-21 17:38:23 -0800

    cleaned up and debugged code

--- tic_tac_toe_game.py
+++ tic_tac_toe_game.py
@@ -1,5 +1,7 @@
-# from IPython.display import clear_output
+#imports
+import random
 
+#Builds a basic user interface in the command line
 def display_board(board):
     print(' ',board[1],' | ',board[2],' | ',board[3])
     print('-----|-----|-----')
@@ -7,198 +9,100 @@ def display_board(board):
     print('-----|-----|-----')
     print(' ',board[7],' | ',board[8],' | ',board[9])
 
-# players = []
 def player_input():
-    
+    #First player chooses their token for the game. 
     choice = False
-    
     while choice == False:
         player1 = input('Player 1 please choose your piece, X or O: ')
-
         if player1 == 'X':
-            player2 = 'O'
             choice = True
             return player1
-#             players.append(player1)
-#             players.append(player2)
         elif player1 == 'O':
-            player2 = 'X'
             choice = True
             return player1
-#             players.append(player1)
-#             players.append(player2)
         else:
             print('Not a valid choice, Please choose X or O: ')
-#     return players
-#     print(player1)
-#     print(player2)
 
 def place_marker(board, marker, position):
     game_update = board
     game_update[position] = marker
     return game_update
 
-# win_cond = {one:[1,2,3],two:[4,5,6],three:[7,8,9],four:[1,4,7],five:[2,5,8],six:[3,6,9],seven:[1,5,9],eight:[3,5,7]}
-
 def win_check(board, marker):
     
     win_cond = {'one':[1,2,3],'two':[4,5,6],'three':[7,8,9],'four':[1,4,7],'five':[2,5,8],'six':[3,6,9],'seven':[1,5,9],'eight':[3,5,7]}
-    reset_board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-
     
     if marker == 'X':
         if [board[win_cond['one'][0]],board[win_cond['one'][1]],board[win_cond['one'][2]]] == ['X','X','X']:
-            print('X wins!!1')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('X wins!! Condition 1')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['two'][0]],board[win_cond['two'][1]],board[win_cond['two'][2]]] == ['X','X','X']:
-            print('X wins!!2')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('X wins!! Condition 2')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['three'][0]],board[win_cond['three'][1]],board[win_cond['three'][2]]] == ['X','X','X']:
-            print('X wins!!3')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('X wins!! Condition 3')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['four'][0]],board[win_cond['four'][1]],board[win_cond['four'][2]]] == ['X','X','X']:
-            print('X wins!!4')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('X wins!! Condition 4')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['five'][0]],board[win_cond['five'][1]],board[win_cond['five'][2]]] == ['X','X','X']:
-            print('X wins!!5')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('X wins!! Condition 5')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['six'][0]],board[win_cond['six'][1]],board[win_cond['six'][2]]] == ['X','X','X']:
-            print('X wins!!6')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('X wins!! Condition 6')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['seven'][0]],board[win_cond['seven'][1]],board[win_cond['seven'][2]]] == ['X','X','X']:
-            print('X wins!!7')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('X wins!! Condition 7')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['eight'][0]],board[win_cond['eight'][1]],board[win_cond['eight'][2]]] == ['X','X','X']:
-            print('X wins!!8')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('X wins!! Condition 8')
+            replaying = replay()
+            return continue_replay(replaying)
         else:
             return 'not yet', 'not, yet'
         
-    if marker == 'O':
+    elif marker == 'O':
         if [board[win_cond['one'][0]],board[win_cond['one'][1]],board[win_cond['one'][2]]] == ['O','O','O']:
-            print('O wins!!1')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('O wins!! Condition 1')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['two'][0]],board[win_cond['two'][1]],board[win_cond['two'][2]]] == ['O','O','O']:
-            print('O wins!!2')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('O wins!! Condition 2')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['three'][0]],board[win_cond['three'][1]],board[win_cond['three'][2]]] == ['O','O','O']:
-            print('O wins!!3')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('O wins!! Condition 3')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['four'][0]],board[win_cond['four'][1]],board[win_cond['four'][2]]] == ['O','O','O']:
-            print('O wins!!4')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('O wins!! Condition 4')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['five'][0]],board[win_cond['five'][1]],board[win_cond['five'][2]]] == ['O','O','O']:
-            print('O wins!!5')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('O wins!! Condition 5')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['six'][0]],board[win_cond['six'][1]],board[win_cond['six'][2]]] == ['O','O','O']:
-            print('O wins!!6')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('O wins!! Condition 6')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['seven'][0]],board[win_cond['seven'][1]],board[win_cond['seven'][2]]] == ['O','O','O']:
-            print('O wins!!7')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('O wins!! Condition 7')
+            replaying = replay()
+            return continue_replay(replaying)
         elif [board[win_cond['eight'][0]],board[win_cond['eight'][1]],board[win_cond['eight'][2]]] == ['O','O','O']:
-            print('O wins!!8')
-            if not replay():
-                game_on = False
-                thanks = 'Thank you for playing!'
-                return game_on, thanks
-            else: 
-                board = reset_board
-                return True, board
+            print('O wins!! Condition 8')
+            replaying = replay()
+            return continue_replay(replaying)
         else:
             return 'not yet', 'not, yet'
-
-import random
 
 def choose_first():
     num = random.randint(1,101)
@@ -222,7 +126,7 @@ def space_check(board, position):
 def full_board_check(board):
     if ' ' not in board:
         #board full
-        print("Board is full! Try again!")
+        print("Board is full!")
         return True
     else:
         return False
@@ -234,10 +138,8 @@ def player_choice(board):
         position = 100
         if pos in ['1','2','3','4','5','6','7','8','9']:
             position = int(pos)
-#         elif position in range(1,10):
             if space_check(board,position):
                 check = True
-#                 print('returnedddddddddddddddddddd')
                 return position
             else:
                 print('This position is taken, please choose another position ')
@@ -261,37 +163,42 @@ def replay():
         else:
             print('Incorrect input, use only Y or N ')
             continue
-    
+
+def switch_sides(player1,player2):
+    if player1 == 'X':
+        player1 = 'O'
+        player2 = 'X'
+        print(f"Player 1 is now {player1}")
+    elif player1 == 'O':
+        player1 = 'X'
+        player2 = 'O'
+        print(f"Player 1 is now {player1}")
+    return player1,player2
+
+def continue_replay(replayed):
+    reset_board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+    if not replayed:
+        thanks = 'Thank you for playing!'
+        return False, thanks
+    else: 
+        board = reset_board
+        return True, board
+        
 ####################################
 #function calls#
 ####################################
     
-    
 print('Welcome to Tic Tac Toe!')
 
-# from IPython.display import clear_output
-
 game_on = True
-
-while game_on == True: 
-    
-#     game_on = True
-
-    board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-    
-    while game_on == True:
+while game_on == True:
         
-#         clear_output
-        
+        #Initial Board
+        board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
         turn = 0
-        
-#         newboard = 1
-#         next_game = 1
-        
         display_board(board)
     
         player1 = player_input() # X or O
-#         print(player1)
         
         if player1 == 'X':
             player2 = 'O'
@@ -300,16 +207,9 @@ while game_on == True:
 
         (play1,play2) = choose_first() # comes back as (1, 2) or (2, 1)
         
-#         print(player1)
-#         print(player2)
-#         print(play1)
-#         print(play2)
-        
+        # the game instance when Player1 is the first move
         while play1 == 1 and game_on == True:
-            
-#             clear_output
-
-#             print(turn)                 
+                             
             if turn % 2 == 0:
                 
                 marker = player1
@@ -323,26 +223,22 @@ while game_on == True:
                 
                 display_board(board)
                 
-#                 print('Loop 1')
-#                 print(f'next_game: {next_game}')
-#                 print(f'newboard: {newboard}') 
-                
-                (next_game, newboard) = win_check(board, marker)
+                next_game, newboard = win_check(board, marker)
+                full = full_board_check(board)
+                if full and next_game == 'not yet':
+                    replaying = replay()
+                    next_game, newboard = continue_replay(replaying)
 
-                
                 if next_game == True:
-#                     print("1111")
                     board = newboard
+                    display_board(board)
+                    player1, player2 = switch_sides(player1,player2)
                 elif next_game == False:
+                    print('End of Game')
+                    #prints thank you for playing
                     print(newboard)
-                    game_on = next_game
-                
-                full_board_check(board)
-                
-                
-#                 if not replay():
-#                     game_on = False
-#                     break
+                    game_on = False
+                    break
                     
             if turn % 2 != 0:
                 
@@ -356,34 +252,25 @@ while game_on == True:
                 space_check(board, position)
                 
                 display_board(board)
-                
-#                 print('Loop 2')
-#                 print(f'next_game: {next_game}')
-#                 print(f'newboard: {newboard}')
-                
-                (next_game, newboard) = win_check(board, marker)
-                
-                if next_game == True:
-#                     print("2222")
-                    board = newboard
-                elif next_game == False:
-                    print(newboard)
-                    game_on = next_game
-                
-                full_board_check(board)
-            
-            else:
-                continue
+                                
+                next_game, newboard = win_check(board, marker)
+                full = full_board_check(board)
+                if full and next_game == 'not yet':
+                    replaying = replay()
+                    next_game, newboard = continue_replay(replaying)
 
-#                 if not replay():
-#                     game_on = False
-#                     break
-                    
-        while play2 == 1 and game_on == True:
-            
-#             clear_output
-                
-#             print(turn)
+                if next_game == True:
+                    board = newboard
+                    display_board(board)
+                    player1, player2 = switch_sides(player1,player2)
+                elif next_game == False:
+                    print('End of Game')
+                    print(newboard)
+                    game_on = False
+                    break            
+
+        # the game instance when Player2 is the first move
+        while play2 == 1 and game_on == True:         
 
             if turn % 2 == 0: 
                 
@@ -397,26 +284,23 @@ while game_on == True:
                 space_check(board, position)
                 
                 display_board(board)
-                
-#                 print('Loop 3')
-#                 print(f'next_game: {next_game}')
-#                 print(f'newboard: {newboard}')
-                
-                (next_game, newboard) = win_check(board, marker)
-                
-                if next_game == True:
-#                     print("3333")
-                    board = newboard
-                elif next_game == False:
-                    print(newboard)
-                    game_on = next_game
-                
-                full_board_check(board)
+                                
+                next_game, newboard = win_check(board, marker)
+                full = full_board_check(board)
+                if full and next_game == 'not yet':
+                    replaying = replay()
+                    next_game, newboard = continue_replay(replaying)
 
-#                 if not replay():
-#                     game_on = False
-#                     break
-                    
+                if next_game == True:
+                    board = newboard
+                    display_board(board)
+                    player1, player2 = switch_sides(player1,player2)
+                elif next_game == False:
+                    print('End of Game')
+                    print(newboard)
+                    game_on = False
+                    break
+
             if turn % 2 != 0:
                 
                 marker = player1
@@ -429,30 +313,19 @@ while game_on == True:
                 space_check(board, position)
                 
                 display_board(board)
-                
-#                 print('Loop 4')
-#                 print(f'next_game: {next_game}')
-#                 print(f'newboard: {newboard}')
-                
-                (next_game, newboard) = win_check(board, marker)
-                
+
+                next_game, newboard = win_check(board, marker)
+                full = full_board_check(board)
+                if full and next_game == 'not yet':
+                    replaying = replay()
+                    next_game, newboard = continue_replay(replaying)
+
                 if next_game == True:
-#                     print("4444")
                     board = newboard
+                    display_board(board)
+                    player1, player2 = switch_sides(player1,player2)
                 elif next_game == False:
+                    print('End of Game')
                     print(newboard)
-                    game_on = next_game
-                
-                full_board_check(board)
-                
-            else:
-                continue
-
-#                 if not replay():
-#                     game_on = False
-#                     break
-
-
-#     break       
-# break 
-    
+                    game_on = False
+                    break
